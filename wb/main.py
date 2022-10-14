a4eb378a50c8578b422810c1f65f4e6c9e888106
TiG000 <43001385+TiG000@users.noreply.github.com>
2022-10-14 21:41:07 +0800

    Update main.py

--- wb/main.py
+++ wb/main.py
@@ -124,11 +124,11 @@ def update_readme(news):
     pth = os.path.join('./_post',f'{yd}-wb.md')
     new_pth = os.path.join('./_post',f'{tm}-wb.md')
     if os.path.exists(new_pth):
-        path = new_pth
+        pth = new_pth
         print('继续更新')
     # lines = f'<!-- BEGIN --> \r\n最后更新时间 {datetime.now()} \r\n![{archive_filepath}]({archive_filepath}.png) \r\n' + lines + '\r\n<!-- END -->'
     lines = f'<!-- BEGIN --> \r\n最后更新时间 {datetime.now()} \r\n' + lines + '\r\n<!-- END -->'
-    content = re.sub(r'<!-- BEGIN -->[\s\S]*<!-- END -->', lines, load(path))
+    content = re.sub(r'<!-- BEGIN -->[\s\S]*<!-- END -->', lines, load(pth))
     save(new_pth, content)
 
 
